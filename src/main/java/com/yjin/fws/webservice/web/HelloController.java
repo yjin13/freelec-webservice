8169dfa0fecfc5a1bcdee74f51180f9c346658b1
yjin13 <bly0319@naver.com>
2020-07-22 23:26:37 +0900

    Lombok 적용

--- src/main/java/com/yjin/fws/webservice/web/HelloController.java
+++ src/main/java/com/yjin/fws/webservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.yjin.fws.webservice.web;
 
+import com.yjin.fws.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount ) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
