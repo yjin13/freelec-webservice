48c02d7fb6a304ecf26c93618d4320bf53720247
yjin13 <bly0319@naver.com>
2020-07-25 21:12:32 +0900

    등록/수정/조회 Test

--- src/test/java/com/yjin/fws/webservice/web/PostApiControllerTest.java
+++ src/test/java/com/yjin/fws/webservice/web/PostApiControllerTest.java
@@ -3,6 +3,7 @@ package com.yjin.fws.webservice.web;
 import com.yjin.fws.webservice.domain.posts.Posts;
 import com.yjin.fws.webservice.domain.posts.PostsRepository;
 import com.yjin.fws.webservice.web.dto.PostsSaveRequestDto;
+import com.yjin.fws.webservice.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
@@ -36,6 +40,8 @@ public class PostApiControllerTest {
         postsRepository.deleteAll();
     }
 
+    private MockMvc mvc;
+
     @Test
     public void Posts_등록된다() throws Exception {
 
@@ -56,6 +62,40 @@ public class PostApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
 
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception {
+
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 
 }
