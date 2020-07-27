5d2988809c4af4c10312a031ec04e97a99e69ac2
yjin13 <bly0319@naver.com>
2020-07-27 23:40:17 +0900

    JPA Auditing 설정

--- src/main/java/com/yjin/fws/webservice/Application.java
+++ src/main/java/com/yjin/fws/webservice/Application.java
@@ -2,7 +2,9 @@ package com.yjin.fws.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
