0027c58571c914c3b3bf04e852a21120b8d2d54f
yjin13 <bly0319@naver.com>
2020-07-29 23:18:55 +0900

    게시글 목록 조회

--- src/main/java/com/yjin/fws/webservice/service/posts/PostsService.java
+++ src/main/java/com/yjin/fws/webservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yjin.fws.webservice.service.posts;
 
 import com.yjin.fws.webservice.domain.posts.Posts;
 import com.yjin.fws.webservice.domain.posts.PostsRepository;
+import com.yjin.fws.webservice.web.dto.PostsListResponseDto;
 import com.yjin.fws.webservice.web.dto.PostsResponseDto;
 import com.yjin.fws.webservice.web.dto.PostsSaveRequestDto;
 import com.yjin.fws.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,6 +39,14 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    // 목록 조회
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/yjin/fws/webservice/web/IndexController.java
+++ src/main/java/com/yjin/fws/webservice/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.yjin.fws.webservice.web;
 
+import com.yjin.fws.webservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
