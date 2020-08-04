bf6a541e1d6c6d33e337a896000b32af9a625d42
yjin13 <bly0319@naver.com>
2020-08-04 22:51:03 +0900

    로그인 기능

---
+++ src/main/java/com/yjin/fws/webservice/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.yjin.fws.webservice.config.auth.dto;
+
+import com.yjin.fws.webservice.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
