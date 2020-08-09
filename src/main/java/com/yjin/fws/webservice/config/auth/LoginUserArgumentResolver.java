3115d92301e407210215c071dd31a925f52b7bda
yjin13 <bly0319@naver.com>
2020-08-09 23:01:14 +0900

    .

--- src/main/java/com/yjin/fws/webservice/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/yjin/fws/webservice/config/auth/LoginUserArgumentResolver.java
@@ -1,4 +1,21 @@
 package com.yjin.fws.webservice.config.auth;
 
-public class LoginUserArgumentResolver {
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+
 }
