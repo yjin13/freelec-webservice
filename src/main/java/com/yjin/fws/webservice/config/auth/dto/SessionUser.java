aa559daba60b0eb056733ebfc396333f9bdacc56
yjin13 <bly0319@naver.com>
2020-08-15 23:02:14 +0900

    naver 로그인

--- src/main/java/com/yjin/fws/webservice/config/auth/dto/SessionUser.java
+++ src/main/java/com/yjin/fws/webservice/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.yjin.fws.webservice.config.auth.dto;
 import com.yjin.fws.webservice.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
