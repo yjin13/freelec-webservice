4ffdb8c3de504a896aca811b10c4ebec5bec7621
yjin13 <bly0319@naver.com>
2020-08-18 23:08:57 +0900

    Test 시큐리티 설정

---
+++ src/main/java/com/yjin/fws/webservice/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.yjin.fws.webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig { }
